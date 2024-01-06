215d98d819a23f1de8f60d4bcb2c040764b8e7cc
Suntiparb <pae@Suntiparbs-Air.ST-244F>
2024-01-06 12:27:03 +0700

    chore: add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [partytown()]
 });
